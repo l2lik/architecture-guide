c3718a14fbbbbc82045d80d3b7ed8c074af2721b
l2lik <aishmirov@gmail.com>
2020-04-15 20:55:39 +0700

    Initial Commit

---
+++ ArchitectureGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureGuide
+//
+//  Created by Aleksei Shmyrov on 15.04.2020.
+//  Copyright © 2020 Aleksei Shmyrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
